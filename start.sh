eee4bacb9daba0b4b0e2c283842df85e05b87f98
Isaac <iico7no2@gmail.com>
2022-08-23 23:52:59 -0500

    Update start.sh comments

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-# This file must have LF line endings!!
+# This file must have LF line endings!! If you see a "no such file or directory"
+# in Docker, it is probably because this file has CRLF line endings.
 
 nginx -c /usr/nginx.conf
 
