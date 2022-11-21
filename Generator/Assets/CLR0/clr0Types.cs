35a6c1cc1340db117f09b541591b9276c1d131d8
lunarsoap5 <justindakotacarter@gmail.com>
2022-11-21 00:16:40 -0600

    Updated formatting and successful implementation of Midna Hair Color PoC

--- Generator/Assets/CLR0/clr0Types.cs
+++ Generator/Assets/CLR0/clr0Types.cs
@@ -138,17 +138,49 @@ namespace TPRandomizer.Assets.CLR0
 
     public class ColorArrays
     {
-        public static readonly int[][] MidnaHairColors = new int[][]
+        public static readonly string[][] MidnaBaseHairColors = new string[][]
         {
             // put the actual RGB values here. Note that there is no value for the default color as we don't want to make the change if the player doesn't want to change the color.
-            new[] { 0, 1, 2, 3, 4, 5, 6 },
-            new[] { 7, 8, 9, 10, 11, 12, 13 },
-            new[] { 0, 1, 2, 3, 4, 5, 6 },
-            new[] { 0, 1, 2, 3, 4, 5, 6 },
-            new[] { 0, 1, 2, 3, 4, 5, 6 },
-            new[] { 0, 1, 2, 3, 4, 5, 6 },
-            new[] { 0, 1, 2, 3, 4, 5, 6 },
-            new[] { 0, 1, 2, 3, 4, 5, 6 },
+            // Colors are lightworld inactive, darkworld inactive, bothworld active
+            new[] { "F5CFF300", "AD7F7F00", "1B002000" }, // Pink
+            new[] { "E4654100", "A13E2200", "21000000" }, // Red
+            new[] { "91830E00", "66500700", "0E0B0000" }, // Yellow
+            new[] { "35795300", "254A2B00", "000E0500" }, // Green
+            new[] { "0072FF00", "00468500", "00082800" }, // Blue
+            new[] { "6F34FF00", "4E208500", "0D003400" }, // Purple
+            new[] { "00000000", "00000000", "1A050000" }, // Brown
+            new[] { "F0F1F100", "A9947E00", "090B0C00" }, // White
+            new[] { "00000000", "00000000", "0B0B0B00" }, // Black
+        };
+
+        public static readonly string[][] MidnaTipsHairColors = new string[][]
+        {
+            // put the actual RGB values here. Note that there is no value for the default color as we don't want to make the change if the player doesn't want to change the color.
+            // Colors are lightworld inactive, darkworld anyactive, lightworld active
+            new[] { "DD00EB00", "DD00C300", "F64CFF00" }, // Pink
+            new[] { "EB000000", "EB000000", "FF4F3A00" }, // Red
+            new[] { "EBDE0000", "EBDE0000", "FFF8BF00" }, // Yellow
+            new[] { "1FEB0000", "1FEB0000", "9AFF8100" }, // Green
+            new[] { "0048EB00", "0048C300", "3A66FF00" }, // Blue
+            new[] { "7B00EB00", "7B00C300", "943EFF00" }, // Purple
+            new[] { "3F1D0B00", "3F1D0900", "59321E00" }, // Brown
+            new[] { "EAEAEA00", "EAEAC200", "F3F3F300" }, // White
+            new[] { "00000000", "00000000", "00000000", }, // Black
+        };
+
+        public static readonly string[][] MidnaGlowHairColors = new string[][]
+        {
+            // put the actual RGB values here. Note that there is no value for the default color as we don't want to make the change if the player doesn't want to change the color.
+            // Colors are bothworld inactive, lightworld active, darkworld active
+            new[] { "003C0002", "00580000", "00E30072", "00F20000", "00E3005F", "00F80000" }, // Pink
+            new[] { "004F0002", "00010000", "00F0003A", "00250000", "00F00030", "008C0000" }, // Red
+            new[] { "00240025", "00020000", "00CB00B7", "00000000", "00CB0099", "00780000" }, // Yellow
+            new[] { "000A0029", "00100000", "000000B6", "006F0000", "00000098", "00B30000" }, // Green
+            new[] { "0016001B", "005D0000", "00000060", "00FF0000", "00000050", "00FF0000" }, // Blue
+            new[] { "00150008", "00790000", "00620000", "00FF0000", "00620000", "00FF0000" }, // Purple
+            new[] { "003C0019", "000E0000", "003F001D", "000B0000", "003F0018", "007E0000" }, // Brown
+            new[] { "00220024", "00240000", "00FF00FF", "00FF0000", "00FF00D5", "00FF0000" }, // White
+            new[] { "00230023", "00230000", "00000000", "00000000", "00000000", "00780000" }, // Black
         };
     }
 }
--- Generator/Assets/SeedData.cs
+++ Generator/Assets/SeedData.cs
@@ -611,7 +611,7 @@ namespace TPRandomizer.Assets
 
             if (fcSettings.midnaHairBaseColor > 0) // we don't want to try to change the hair color if the value is default
             {
-                int[] midnaBaseOffsets = { 0, 1, 2, 3, 4, 5 };
+                int[] midnaBaseOffsets = { 0xA438, 0xA424, 0xA434 }; //lightworld inactive, darkworld inactive, bothworld active
                 for (int i = 0; i < midnaBaseOffsets.Length; i++) // since we don't need to have any values for default, the values at array index 0 are for color 1, etc.
                 {
                     listOfRELReplacements.AddRange(Converter.GcBytes((UInt16)0x3)); // replacement type
@@ -621,9 +621,33 @@ namespace TPRandomizer.Assets
                     listOfRELReplacements.AddRange(
                         Converter.GcBytes(
                             (UInt32)
-                                Assets.CLR0.ColorArrays.MidnaHairColors[
-                                    fcSettings.midnaHairBaseColor - 1
-                                ][i]
+                                uint.Parse(
+                                    Assets.CLR0.ColorArrays.MidnaBaseHairColors[
+                                        fcSettings.midnaHairBaseColor - 1
+                                    ][i],
+                                    System.Globalization.NumberStyles.HexNumber
+                                )
+                        )
+                    );
+                    count++;
+                }
+
+                int[] midnaGlowOffsets = { 0xA41C, 0xA420, 0xA440, 0xA444, 0xA42C, 0xA430 }; //bothworld inactive, lightworld active, darkworld active
+                for (int i = 0; i < midnaGlowOffsets.Length; i++) // since we don't need to have any values for default, the values at array index 0 are for color 1, etc.
+                {
+                    listOfRELReplacements.AddRange(Converter.GcBytes((UInt16)0x3)); // replacement type
+                    listOfRELReplacements.AddRange(Converter.GcBytes((UInt16)0xFF)); // stageIDX
+                    listOfRELReplacements.AddRange(Converter.GcBytes((UInt32)0x33)); // moduleID
+                    listOfRELReplacements.AddRange(Converter.GcBytes((UInt32)midnaGlowOffsets[i])); // offset
+                    listOfRELReplacements.AddRange(
+                        Converter.GcBytes(
+                            (UInt32)
+                                uint.Parse(
+                                    Assets.CLR0.ColorArrays.MidnaGlowHairColors[
+                                        fcSettings.midnaHairBaseColor - 1
+                                    ][i],
+                                    System.Globalization.NumberStyles.HexNumber
+                                )
                         )
                     );
                     count++;
@@ -632,7 +656,7 @@ namespace TPRandomizer.Assets
 
             if (fcSettings.midnaHairTipsColor > 0) // we don't want to try to change the hair color if the value is default
             {
-                int[] midnaTipOffsets = { 0, 1, 2, 3, 4, 5 };
+                int[] midnaTipOffsets = { 0xA43C, 0xA428, 0xA448 }; // lightworld inactive, darkworld anyactive, lightworld active
                 for (int i = 0; i < midnaTipOffsets.Length; i++) // since we don't need to have any values for default, the values at array index 0 are for color 1, etc.
                 {
                     listOfRELReplacements.AddRange(Converter.GcBytes((UInt16)0x3)); // replacement type
@@ -642,9 +666,12 @@ namespace TPRandomizer.Assets
                     listOfRELReplacements.AddRange(
                         Converter.GcBytes(
                             (UInt32)
-                                Assets.CLR0.ColorArrays.MidnaHairColors[
-                                    fcSettings.midnaHairTipsColor - 1
-                                ][i]
+                                uint.Parse(
+                                    Assets.CLR0.ColorArrays.MidnaTipsHairColors[
+                                        fcSettings.midnaHairTipsColor - 1
+                                    ][i],
+                                    System.Globalization.NumberStyles.HexNumber
+                                )
                         )
                     ); // replacement value
                     count++;
