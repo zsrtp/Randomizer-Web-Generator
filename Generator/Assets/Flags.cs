07921ef79d4ce63831d90454d10a18c6eaf7b474
lunarsoap5 <justindakotacarter@gmail.com>
2023-06-28 19:07:32 -0500

    Add flag for squidna CS to be set by default

--- Generator/Assets/Flags.cs
+++ Generator/Assets/Flags.cs
@@ -72,6 +72,7 @@ namespace TPRandomizer.Assets
             { 0x9, 0x76 }, // Jovani Chest CS 2
             { 0x9, 0x7F }, // Open Chest to Jovani
             { 0x9, 0x7E }, // Jovani Chest CS
+            { 0x9, 0x50 }, // Set flag for Midna breaking Barrier CS.
             { 0x3, 0xA7 }, // Unlock Jumps to top of Sanctuary
             { 0x3, 0x9A }, // Kakariko Village intro CS.
             { 0xA, 0x7F }, // Mirror Raised Cutscene Flag (Places Boar at desert entrance)
