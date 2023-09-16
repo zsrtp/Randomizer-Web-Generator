b33d68589c97fdd9898debb91172042dcd2b07eb
lunarsoap5 <40438826+lunarsoap5@users.noreply.github.com>
2023-09-15 20:00:23 -0500

    Add code that was removed in PR#36 to properly display and handle settings strings (#44)
    * Add code that was removed in previous PR to get the generator back to a functional state.
    
    * Remove duplicate code

--- packages/client/script.js
+++ packages/client/script.js
@@ -214,6 +214,7 @@ for (
     [j].addEventListener('click', setSettingsString);
 }
 
+var settingsLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ23456789';
 document.getElementById('logicRulesFieldset').onchange = setSettingsString;
 document.getElementById('gameRegionFieldset').onchange = setSettingsString;
 document.getElementById('seedNumberFieldset').onchange = setSettingsString;
@@ -310,6 +311,14 @@ document
 document
   .getElementById('modifyShopModelsCheckbox')
   .addEventListener('click', setSettingsString);
+
+  document
+  .getElementById('importSettingsStringButton')
+  .addEventListener('click', importSettingsString);
+
+function importSettingsString() {
+  parseSettingsString(document.getElementById('settingsStringTextbox').value);
+}
 
 function setSettingsString() {
   var settingsStringRaw = [];
