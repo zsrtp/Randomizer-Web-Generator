7647aa3a12b15ae39cfe5d2ff2b48520f5c8cc39
Isaac <iico7no2@gmail.com>
2024-04-24 00:12:34 -0500

    Remove Snowpeak sometimes hint

--- Generator/Hints/HintSettings.cs
+++ Generator/Hints/HintSettings.cs
@@ -131,7 +131,7 @@ namespace TPRandomizer.Hints.Settings
                 "Outside Arbiters Grounds Lantern Chest",
                 // "Snowboard Racing Prize",
 
-                "Snowpeak Cave Ice Lantern Chest",
+                // "Snowpeak Cave Ice Lantern Chest", // Seemed kind of worthless when it showed up
                 "Forest Temple Gale Boomerang",
                 "Lakebed Temple Deku Toad Chest",
                 "Arbiters Grounds Death Sword Chest",
